3d8aa21d2dca90610b10c90f063cf65d8fcb8b9c
Mirko Garozzo <mirkogarozzo@hotmail.it>
2019-07-24 20:27:49 +0200

    Removed old files

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const extensionPrefix: string = "mythx";
-
-export const MYTHX_USER_STAGING: string = '0x9b4a8d7e6f4bf844fb3ba1b323365dec62230c29'
-export const MYTHX_PASSWORD_STAGING: string = '7nJJ1?YDC71Mrv'
-
-export const MYTHX_USER: string = '0x79b483371e87d664cd39491b5f06250165e4b184'
-export const MYTHX_PASSWORD: string = 'Abcdefg1!'
