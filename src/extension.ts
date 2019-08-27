2406c2835f397f9e15977d27817ce5ea7ca37600
Mirko Garozzo <mirkogarozzo@hotmail.it>
2019-08-27 12:37:02 -0500

    Cleared unused logs

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,8 @@ import { analyzeContract } from "./commands/analyzeContract";
 let diagnosticsCollection: vscode.DiagnosticCollection
 
 export async function activate(context: vscode.ExtensionContext) {
-    const {window} = vscode
     ext.context = context;
     ext.outputChannel = vscode.window.createOutputChannel("MythX");
-
-    const foo = vscode.workspace.getConfiguration('solidity');
-    console.log(foo)
 
     diagnosticsCollection = vscode.languages.createDiagnosticCollection('mythx');
 
