c0d89a2e621975252f264eee73b4a5a5df643629
Mirko Garozzo <mirkogarozzo@hotmail.it>
2019-10-03 14:03:12 -0500

    Added error handling for full mode

--- src/commands/runFullMode.ts
+++ src/commands/runFullMode.ts
@@ -12,55 +12,62 @@ let mythx: Client
 export async function runFullMode(fileUri: string): Promise<void> {
 	let contractName;
 
-	await vscode.extensions.getExtension("JuanBlanco.solidity").activate().then(
-		async (active) => {
-			vscode.commands.executeCommand("solidity.compile.active").then(
-				async done => {
-					if(!done) {
-						throw new Error(`MythX: Error with solc compilation.`)
-					} else {
-						const credentials: Credentials = await getCredentials();
-						mythx = new Client(credentials.ethAddress, credentials.password, 'mythXvsc');
-
-						await mythx.login();
-
-						const documentObj = await vscode.workspace.openTextDocument(fileUri)
-    					const fileContent = documentObj.getText();
-
-						// Get contract names array for dropdown
-						const contractNames = fileContent.match(/(?<=contract\s)(\w+)(?=\s*{)/g);
-
-						await window.showQuickPick(contractNames, {
-							canPickMany: false,
-							placeHolder: 'Contract Name (please select main contract):'
-						}).then(
-							value => {
-								if(value === undefined) {
-									throw new Error('Contract Name cancelled. Please re-run analysis.');
-								}
-								contractName = value;
+		await vscode.extensions.getExtension("JuanBlanco.solidity").activate().then(
+			async (active) => {
+				vscode.commands.executeCommand("solidity.compile.active").then(
+					async done => {
+						try {
+							if(!done) {
+								throw new Error(`MythX: Error with solc compilation.`)
+							} else {
+								const credentials: Credentials = await getCredentials();
+								mythx = new Client(credentials.ethAddress, credentials.password, 'mythXvsc');
+		
+								await mythx.login();
+		
+								const documentObj = await vscode.workspace.openTextDocument(fileUri)
+								const fileContent = documentObj.getText();
+		
+								// Get contract names array for dropdown
+								const contractNames = fileContent.match(/(?<=contract\s)(\w+)(?=\s*{)/g);
+		
+								await window.showQuickPick(contractNames, {
+									canPickMany: false,
+									placeHolder: 'Contract Name (please select main contract):'
+								}).then(
+									value => {
+										if(value === undefined) {
+											throw new Error('Contract Name cancelled. Please re-run analysis.');
+										}
+										contractName = value;
+									}
+								)
+		
+								const requestObj: AnalyzeOptions = await getAstData(contractName, fileContent, 'full');
+								const analyzeRes = await mythx.analyze(
+									requestObj,
+								);
+		
+								const {uuid} = analyzeRes;
+	
+								vscode.window.showInformationMessage(
+									`Your analysis has been submitted! Please see your results at
+									https://dashboard.mythx.io/#/console/analyses/${uuid}`, 'Dismiss'
+								).then(
+									x => {return}
+								)
+								return;
 							}
-						)
 
-						const requestObj: AnalyzeOptions = await getAstData(contractName, fileContent, 'full');
-
-						const analyzeRes = await mythx.analyze(
-							requestObj,
-						);
-
-						const {uuid} = analyzeRes;
-						vscode.window.showInformationMessage(
-							`Your analysis has been submitted! Please see your results at
-							https://dashboard.mythx.io/#/console/analyses/${uuid}`, 'Dismiss'
-						).then(
-							x => {return}
-						)
-						return;
+						}  catch(err) {
+							vscode.window.showErrorMessage(
+								`MythXvsc: ${err}`
+							)
+						}
 					}
-				}
-			)
-		},
-		(err) =>{ throw new Error(`MythX: Error with solc compilation. ${err}`) }
-	)
+				)
+			},
+			(err) =>{ throw new Error(`MythX: Error with solc compilation. ${err}`) }
+		)
 	
 }
